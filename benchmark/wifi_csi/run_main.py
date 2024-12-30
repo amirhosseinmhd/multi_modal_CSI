Fixing data leakage problem. Adding master splitter function which ensure there is no leackage from training set of pretraining to test of finetuning.

benchmark/wifi_csi/run_main.py
@@ -8,6 +8,7 @@ import json
 import argparse
 from logging import raiseExceptions
 
+import numpy as np
 from sklearn.model_selection import train_test_split
 #
 from model import *
@@ -16,6 +17,56 @@ from load_data import load_data_x, load_data_y, encode_data_y
 from utils import *
 #
 ##
+def mater_splitter(preset, var_task, var_model, var_users):
+    env_data_x_train = []
+    env_data_x_test = []
+    env_data_y_train = []
+    env_data_y_test = []
+    for env in preset["data"]["environment"]:
+        data_pd_y = load_data_y(preset["path"]["data_y"],
+                                var_environment=[env],
+                                var_wifi_band=preset["data"]["wifi_band"],
+                                var_num_users=var_users)
+        #
+        var_label_list = data_pd_y["label"].to_list()
+        #
+        ## load CSI amplitude
+        X = load_data_x(preset["path"]["data_x"], var_label_list)
+
+
+        y = encode_data_y(data_pd_y, var_task)
+
+        if var_model == "THAT_MULTI_HEAD":
+            y = reduce_dataset(y)  # CHECKKKKKKKK HEREEEEEE
+        elif var_model == "THAT_ENCODER" or var_model == "DETR":
+            y = reduce_dataset(y, preset["nn"]["num_obj_queries"])  # CHECKKKKKKKK HEREEEEEE
+        elif var_model == "THAT_COUNT_CONSTRAINED":
+            y_red = reduce_dataset(y)
+            y = y_red.sum(axis=1)
+        else:
+            pass
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.2,
+                                                            shuffle=True,
+                                                            random_state=103)
+        # np.random.randint()
+        env_data_x_train.append(X_train)
+        env_data_x_test.append(X_test)
+        env_data_y_train.append(y_train)
+        env_data_y_test.append(y_test)
+
+
+    data_x_train = np.concatenate(env_data_x_train, axis = 0)
+    data_x_test = np.concatenate(env_data_x_test, axis = 0)
+    data_y_train = np.concatenate(env_data_y_train, axis = 0)
+    data_y_test = np.concatenate(env_data_y_test, axis = 0)
+
+
+    return data_x_train, data_x_test, data_y_train, data_y_test
+
+
+
 def parse_args():
     """
     [description]
@@ -28,7 +79,7 @@ def parse_args():
     var_args.add_argument("--model", default = preset["model"], type = str)
     var_args.add_argument("--task", default = preset["task"], type = str)
     var_args.add_argument("--repeat", default = preset["repeat"], type = int)
-    var_args.add_argument("--users", default="1,2,3,4,5", type=str, help="Comma-separated list of user IDs")
+    var_args.add_argument("--users", default="0, 1,2,3,4,5", type=str, help="Comma-separated list of user IDs")
     #
     return var_args.parse_args()
 
@@ -47,35 +98,20 @@ def run():
     var_model = var_args.model
     var_repeat = var_args.repeat
     var_users = [u.strip() for u in var_args.users.split(',')]
-    # preset["data"]["num_users"]  = var_users
-    #
-    ## load annotation file as labels
-    data_pd_y = load_data_y(preset["path"]["data_y"],
-                            var_environment = preset["data"]["environment"],
-                            var_wifi_band = preset["data"]["wifi_band"],
-                            var_num_users = preset["data"]["num_users"])
-    #
-    var_label_list = data_pd_y["label"].to_list()
-    #
-    ## load CSI amplitude
-    data_x = load_data_x(preset["path"]["data_x"], var_label_list)
-    #
-    data_y = encode_data_y(data_pd_y, var_task)
-    #
-    if var_model == "THAT_MULTI_HEAD":
-        data_y = reduce_dataset(data_y) # CHECKKKKKKKK HEREEEEEE
-    if var_model == "THAT_ENCODER" or var_model == "DETR":
-        data_y = reduce_dataset(data_y, preset["nn"]["num_obj_queries"]) # CHECKKKKKKKK HEREEEEEE
 
-    elif var_model == "THAT_COUNT_CONSTRAINED":
-        data_y_red = reduce_dataset(data_y)
-        data_y = data_y_red.sum(axis=1)
-    ## a training set (80%) and a test set (30%)
-    data_train_x, data_test_x, data_train_y, data_test_y = train_test_split(data_x, data_y,
-                                                                            test_size = 0.2,
-                                                                            shuffle = True,
-                                                                            random_state = 103)
+    preset["repeat"] = 1 if not preset["pretrained_path"] else preset["repeat"] # if we want to pretrain the model we
+    #                                                                           # need only one repeat
+
+    # Ensuring there is no data leakage while doing splits.
+    data_train_x, data_test_x, data_train_y, data_test_y = mater_splitter(preset, var_task, var_model, var_users)
     #
+
+    ## a training set (80%) and a test set (30%)
+    # data_train_x, data_test_x, data_train_y, data_test_y = train_test_split(data_x, data_y,
+    #                                                                         test_size = 0.2,
+    #                                                                         shuffle = True,
+    #                                                                         random_state = 103)
+    # #
     ## select a WiFi-based model
     if var_model == "ST-RF": run_model = run_strf
     #