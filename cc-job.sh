68354baa5a5813c4e3d8bc1552e6e97df97acea2
amirmhd <mdi.amirhossein@gmail.com>
2025-01-28 13:05:31 -0800

    cc-job.sh Does not copy the data from local device to temp node. It reads directly from there.

--- cc-job.sh
+++ cc-job.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 #SBATCH --mem=64G
 #SBATCH --nodes=1
-#SBATCH --time=1:30:0
+#SBATCH --time=30:30:0
 #SBATCH --mail-user=mdi.amirhossein@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --gpus-per-node=1
 #SBATCH --job-name=csi_job
 #SBATCH --output=timeStream-%j.out
 
-export OUTFILE_NAME=timeStream-%j.out
+export OUTFILE_NAME="timeStream-${SLURM_JOB_ID}"
 
 # Define important directories
 PROJECT_DIR=/home/amirmhd/projects/def-hinat/amirmhd/multi_modal_CSI
 CODE_DIR=benchmark/wifi_csi
-DATA_DIR=dataset
+# DATA_DIR=dataset
 
 # Create directory structure in SLURM_TMPDIR
 echo "Copying code and data to temporary directory..."
 mkdir -p $SLURM_TMPDIR/$CODE_DIR
-mkdir -p $SLURM_TMPDIR/$DATA_DIR
+# mkdir -p $SLURM_TMPDIR/$DATA_DIR
 
 # Copy data to temporary directory for faster execution
-cp -r $PROJECT_DIR/$DATA_DIR/* $SLURM_TMPDIR/$DATA_DIR/
-echo "Data copied to temporary directory."
+# cp -r $PROJECT_DIR/$DATA_DIR/* $SLURM_TMPDIR/$DATA_DIR/
+# echo "Data copied to temporary directory."
 # Copy code to temporary directory for faster execution
-echo "Copying code to temporary directory..."
+# echo "Copying code to temporary directory..."
 cp -r $PROJECT_DIR/$CODE_DIR/* $SLURM_TMPDIR/$CODE_DIR/
 cd $SLURM_TMPDIR/$CODE_DIR
-
-echo "Lets see whats inside the temporary directory"
-echo $(ls -R $SLURM_TMPDIR)
 
 module purge
 module load python/3.11.5 scipy-stack
@@ -41,21 +38,22 @@ echo "Python version: $(python --version)"
 
 
 # Update your data paths in the Python script to use SLURM_TMPDIR
-export DATA_PATH=$SLURM_TMPDIR/$DATA_DIR
-export NUM_DECODER_LAYERS=1
-export NUM_QUERIES=8
+# export DATA_PATH=$SLURM_TMPDIR/$DATA_DIR
+export AUX_LOSS=0.5
+# export NUM_QUERIES=5
 
 # Now we change the preset accordingly
 python config_modifier.py preset.py modified_preset.py
 # just to verify:
 cat modified_preset.py
+rm preset.py
+mv modified_preset.py preset.py
 echo "starting the main script"
 python run_main.py
 
 
 if [ -d "results" ]; then
     echo "Copying results back to project directory..."
-    mkdir $PROJECT_DIR/$OUTFILE_NAME
     cp -r results $PROJECT_DIR/$OUTFILE_NAME
 fi
 
